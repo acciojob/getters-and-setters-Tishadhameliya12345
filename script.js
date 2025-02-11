45f7e4375e33d502604eea1d49046a5c69d12aa3
Tishadhameliya12345 <159407986+Tishadhameliya12345@users.noreply.github.com>
2025-02-11 12:53:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,31 +5,30 @@ class Person {
     this._age = age;
   }
 
-  get name() {
+	get name() {
     return this._name;
   }
 
-  get age() {
-    return this._age;
-  }
-
-  set age(age) {
-    this._age = age;
-  }
+	set age(age){
+		this._age = age;
+	}
 }
 
 class Student extends Person {
-	  study() {
-    console.log(${this.name} is studying);
-  }
+	study() {
+	    console.log(`${this.name} is studying`);
+	  }
+
 }
 
 class Teacher extends Person {
 	teach() {
-    console.log(${this.name} is teaching);
-  }
+	    console.log(`${this._name} is teaching`);
+	  }
+
 }
 
+// Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
